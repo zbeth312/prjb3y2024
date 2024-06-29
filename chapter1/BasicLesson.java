ac3c21fa2879f728504af09f6af6abac10e403d2
zbeth312 <zbeth.graphics312@gmail.com>
2024-06-29 01:41:05 +0000

    done

---
+++ chapter1/BasicLesson.java
@@ -0,0 +1,42 @@
+package chapter1;
+public class BasicLesson{
+
+    
+}
+class Main {
+    //public static void main(string[] args) {}
+    //static public void main(string[] args) {}
+    //public static void main(string[] marc) {}
+    //psvm
+    public static void main(String[] args) {
+        Client c1 = new Client();
+        System.out.println("Hello World");
+    }
+        
+    }
+
+/* This is a
+ * muliple line
+ * comment
+ */
+
+/**
+ * documentation comment
+ * client object
+ * @author Marco Yimyaem
+ * @param not applicable
+ */
+class Client{
+    String Lname; // clients last name
+    String Fname;
+    String Address; 
+    int idNum;
+    String acctType;
+}
+
+
+class Transactions{
+    String transType;
+    int amount;
+    int idNum;
+}
