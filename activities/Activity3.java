be2ee3d52eaaf2f9c744278b82f7a22ab3ea91ea
zbeth312 <zbeth.graphics312@gmail.com>
2024-07-09 13:39:17 +0000

    DONE activity 3

---
+++ activities/Activity3.java
@@ -0,0 +1,22 @@
+package activities;
+
+public class Activity3 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String Lastname = "Elizabeth";
+        
+        System.out.println(Lastname.length());
+        System.out.println(Lastname.toUpperCase());
+        System.out.println(Lastname.toLowerCase());
+        System.out.println(Lastname.charAt(0));
+        System.out.println(Lastname.charAt(8));
+        System.out.println(Lastname.substring(2, 7));
+
+
+
+
+    }
+    
+}
